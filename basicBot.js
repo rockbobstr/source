1f3620a9abe24bd26d103a390f53e71202b3c107
SirAtlass <54083539+SirAtlass@users.noreply.github.com>
2019-08-13 18:21:51 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3203,17 +3203,41 @@
                     }
                 }
             },
-
-            beckyCommand: {
+         
+              beckyCommand: {
                 command: 'becky',
                 rank: 'user',
-                type: 'exact',
+                type: 'startsWith',
+                getCookie: function(chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.beckys.length);
+                    return basicBot.chat.beckys[c];
+                },
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
-                    else if (!basicBot.commands.executable(this.rank, chat)) return void(0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        return API.sendChat(subChat(basicBot.chat.becky, {
-                                   becky: this.getbecky()))
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.beckys);
+                            return false;
+                        } else {
+                                if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.beckys, {
+                                    name: name
+                                }));
+                            } else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.beckys, {
+                                    name: name
+                                }));
+                            } else {
+                                return API.sendChat(subChat(basicBot.chat.becky, {
+                                    nameto: user.username,
+                                    namefrom: chat.un,
+                                    becky: this.getbecky()
+                                }));
+                            }
                         }
                     }
                 }
